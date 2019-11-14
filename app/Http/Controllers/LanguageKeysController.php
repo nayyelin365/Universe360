fcab276565cbebfe2a3a9bced507b6b54e36243e
Nay Zin Htoo <nayzinhtoo2018@gmail.com>
2019-11-14 17:01:55 +0630

    NZH: Language's Keys Api

---
+++ app/Http/Controllers/LanguageKeysController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Service\LanguagesService;
+
+class LanguageKeysController extends Controller
+{
+    function __construct()
+    {
+        $this->LanguageKeysService = new LanguageKeysService();
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = $this->LanguageKeysService->get_all();
+        return dd($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Service/LanguageKeysService.php
@@ -0,0 +1,35 @@
+<?php 
+namespace App\Service;
+use App\Model\LanguageKeysModel;
+use DB;
+
+	class LanguageKeysService 
+	{
+	    public function get_all()
+		{
+	        /*$data = DB::table('language_keys')
+			        ->join('languages', 'language_keys.language_id', '=', 'languages.id')
+			        ->join('keys', 'language_keys.key_id', '=', 'keys.id')
+			        ->select('keys.key_name as key_name', 'languages.language_name as language_name','language_keys.key_description as key_description', 'language_keys.language_audio as language_audio')
+			        ->get();
+			return $data;*/
+			return LanguageKeysModel::with('keys','languages')->get();
+		}
+		public function get_language_keys_according_to_language()
+		{
+			$languages=DB::table('languages')
+					->select( 'languages.id as language_id','languages.language_name as language_name')
+			        ->get();
+			foreach ($languages as $language) 
+			{
+				$data[$language->language_name] = DB::table('language_keys')
+			        ->join('languages', 'language_keys.language_id', '=', 'languages.id')
+			        ->join('keys', 'language_keys.key_id', '=', 'keys.id')
+			        ->where('language_keys.language_id', '=',$language->language_id)
+			        ->select('keys.key_name as key','language_keys.key_description as value', 'language_keys.language_audio as audio')
+			        ->get();
+			}
+			return $data;
+		}
+	}
+?>
